246be8e2d4f155cb87fe9c61466bdce202d979ed
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-18 20:38:43 -0300

    Aula 220 - Criação de VendedorDaoJDBC e FabricaDao

--- Projeto_JDBC-DAO/src/application/Programa.java
+++ Projeto_JDBC-DAO/src/application/Programa.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.FabricaDao;
+import model.dao.VendedorDao;
 import model.entities.Departamento;
 import model.entities.Vendedor;
 
@@ -11,6 +13,8 @@ public class Programa {
 		Departamento obj = new Departamento(1, "Books");
 		
 		Vendedor vend = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		
+		VendedorDao vendeDao = FabricaDao.criarVendedorDao();
 	
 		System.out.println(vend);
 
