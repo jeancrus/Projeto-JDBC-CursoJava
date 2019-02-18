cad4aa983f8006bd2d19e22725518727c581aa8e
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-18 19:56:10 -0300

    Aula 218 PROJETO - Criação de entidade vendedor

--- Projeto_JDBC-DAO/src/application/Programa.java
+++ Projeto_JDBC-DAO/src/application/Programa.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		Departamento obj = new Departamento(1, "Books");
-		System.out.println(obj);
+		
+		Vendedor vend = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+	
+		System.out.println(vend);
 
 	}
 
