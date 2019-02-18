4557d7af5764692ad218e7c518b82d3b026ef2ac
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-18 19:12:48 -0300

    Aula 217 PROJETO - Criação de classe entidade departamento

---
+++ Projeto_JDBC-DAO/src/application/Programa.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Departamento obj = new Departamento(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
