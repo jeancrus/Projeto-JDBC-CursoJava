3ccfd53e9fc3f25dd64438ae4d5f8b0ea4c83aad
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-20 16:32:19 -0300

    Aula 221 - Implementação do findById

--- Projeto_JDBC-DAO/src/application/Programa.java
+++ Projeto_JDBC-DAO/src/application/Programa.java
@@ -1,20 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.FabricaDao;
 import model.dao.VendedorDao;
-import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		Departamento obj = new Departamento(1, "Books");
-		
-		Vendedor vend = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
 		
 		VendedorDao vendeDao = FabricaDao.criarVendedorDao();
+		
+		Vendedor vend = vendeDao.findById(4);
 	
 		System.out.println(vend);
 
--- Projeto_JDBC-DAO/src/model/dao/impl/VendedorDaoJDBC.java
+++ Projeto_JDBC-DAO/src/model/dao/impl/VendedorDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.VendedorDao;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class VendedorDaoJDBC implements VendedorDao {
-
+	
+	private Connection conn;
+	
+	public VendedorDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void inserir(Vendedor obj) {
 		// TODO Auto-generated method stub
@@ -27,8 +40,41 @@ public class VendedorDaoJDBC implements VendedorDao {
 
 	@Override
 	public Vendedor findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE seller.Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Departamento dep = new Departamento();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+				Vendedor obj = new Vendedor();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartamento(dep);
+				return obj;
+			}
+			return null;
+			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
+		
 	}
 
 	@Override
