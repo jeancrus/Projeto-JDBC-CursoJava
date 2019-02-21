ab13577d0d3ccb81d80cad6468dfc69aa6456151
Jean Crus <jean.carlos@pucpr.edu.br>
2019-02-20 21:02:37 -0300

    Aula 223 - Implementação findByDepartamento

--- Projeto_JDBC-DAO/src/application/Programa.java
+++ Projeto_JDBC-DAO/src/application/Programa.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.FabricaDao;
 import model.dao.VendedorDao;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Programa {
@@ -13,8 +16,14 @@ public class Programa {
 		
 		System.out.println("==== TESTE 1: seller findById ==== ");
 		Vendedor vend = vendeDao.findById(3);
-	
 		System.out.println(vend);
+		
+		System.out.println("\n==== TESTE 2: seller findByDepartamento ==== ");
+		Departamento departamento = new Departamento(2, "a");
+		List<Vendedor> list = vendeDao.findByDepartamento(departamento);
+		for (Vendedor obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
